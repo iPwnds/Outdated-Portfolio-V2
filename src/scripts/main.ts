f23cacf0a1e9d06092594b44434d8493626aae01
Florian <florian.braun.2005@gmail.com>
2022-07-25 16:04:29 +0200

    adding a browser detect feature

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -3,3 +3,33 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+function BrowserDetect(){
+
+	let userAgent = navigator.userAgent;
+    let browserName;
+         
+        if(userAgent.match(/chrome|chromium|crios/i)){
+             browserName = "chrome";
+        	}else if(userAgent.match(/firefox|fxios/i)){
+             browserName = "firefox";
+           	}  else if(userAgent.match(/safari/i)){
+             browserName = "safari";
+           	}else if(userAgent.match(/opr\//i)){
+             browserName = "opera";
+           	} else if(userAgent.match(/edg/i)){
+             browserName = "edge";
+           	}else{
+             browserName="No browser detection";
+           	}
+
+        console.log(browserName)
+
+    	if (browserName == "chrome") {
+    	  	 document.write("<link rel=\"stylesheet\" href=\"src/styles/style.css\" >");
+   			}else if (browserName == "firefox") {
+    	  	 document.write("<link rel=\"stylesheet\" href=\"src/styles/style.sass\" >");
+    		}else {
+    	  	 document.write("<link rel=\"stylesheet\" href=\"src/styles/style.css\" >");
+    		}
+}
