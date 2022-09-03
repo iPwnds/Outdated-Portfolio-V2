6c603bc470e98414636df8ce557049f11d220e19
Florian Braun <florian.braun.2005@gmail.com>
2022-09-03 10:18:07 +0200

    adding the ChangeDate function to solve issue #2

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,20 @@
+function ChangeDate(){
+	let dateEl = document.getElementById("current-year")
+	dateEl.innerText = new Date().getFullYear();
+	console.log('hi')
+}
+
+function SideBar(){
+
+	let progress = document.getElementById('progressbar');
+	let totalHeight = document.body.scrollHeight - window.innerHeight;
+	
+		window.onscroll = function () {
+  			let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  			progress.style.height = progressHeight + "%";
+		}
+}
+
 (function($) { "use strict";
 
 	
@@ -39,14 +56,3 @@
 	});
 	
 })(jQuery);
-
-function SideBar(){
-
-	let progress = document.getElementById('progressbar');
-	let totalHeight = document.body.scrollHeight - window.innerHeight;
-	
-		window.onscroll = function () {
-  			let progressHeight = (window.pageYOffset / totalHeight) * 100;
-  			progress.style.height = progressHeight + "%";
-		}
-}
