8eafa07569dae4f138ea8a3061384a35137d0b65
iPwnds <florian.braun.2005@gmail.com>
2023-01-12 00:32:30 +0100

    adding a little quote to the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
 <div id="progressbar"></div>
   <header>
     <div class="topnav" data-aos="fade-in">
+      <p class="quote rainbow" data-aos="fade-in">Never Give Up</p>
       <a href="index.html" data-aos="fade-in"><b>// Florian's Portfolio</b></a>
       <a href="about.html" data-aos="fade-in">// About</a>
       <a href="projects.html" data-aos="fade-in">// Projects</a>
@@ -13,14 +14,11 @@
 <style scoped>
   header {
     overflow: hidden;
+    display: flex;
     justify-content: center;
     align-items: center;
-    display: flex;
     text-shadow: none;
-    padding-left: 2vw;
-    padding-right: 2vw;
-    padding-top: 2vw;
-    padding-bottom: 10vh;
+    padding: 2vw 2vw 10vh 2vw;
   }
 
   .topnav {
@@ -28,38 +26,23 @@
     border-radius: 20px;
     line-height: 2.5;
     text-shadow: none;
-    padding: 1vw;
-    justify-content: center;
-    align-items: center;
+    padding: 0.25vw 1vw 1vw 0.75vw;
   }
 
   header a {
     float: left;
     color: var(--midnight-blue-400);
-    text-align: center;
-    padding-left: 3vw;
-    padding-right: 3vw;
-    padding-top: 0.75vw;
-    padding-bottom: 0.75vw;
+    padding: 0.75vw 3vw 0.75vw 3vw;
     line-height: 2.5;
     text-decoration: none;
     font-size: 1.5vw;
-    justify-content: center;
-    align-items: center;
   }
 
   header a:hover {
     background-color: var(--midnight-blue-600);
     border-radius: 20px;
-    line-height: 2.5;
-    padding-right: 3vw;
-    padding-left: 3vw;
-    padding-top: 0.75vw;
-    padding-bottom: 0.75vw;
     text-shadow: none;
     box-shadow: 3px 2px 2px var(--midnight-blue-800);
-    justify-content: center;
-    align-items: center;
   }
 
   @keyframes animate {
@@ -82,7 +65,7 @@
 	  animation: animate 10s linear infinite;
   }
 
-  #progressbar:before {
+  #progressbar :before {
   	content: '';
   	position: absolute;
   	top: 0;
@@ -91,6 +74,19 @@
   	height: 100%;
   	background: linear-gradient(to top, #62efab, #62efab);
   	filter: blur(10px);
+  }
+
+  .quote {
+    text-align: left;
+    font-size: 0.5vw;
+  }
+
+  .rainbow {
+    background: linear-gradient(to right, #62efab 5%, #F2EA7D 15%, #F2EA7D 25%, #FF8797 35%, #FF8797 45%, #e1a4f4 55%, #e1a4f4 65%, #82fff4 75%, #82fff4 85%, #62efab 95%);
+    background-size: 200%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: bar 15s linear infinite;
   }
 </style>
 
