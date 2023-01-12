8648e9d053e7c7086a113c1cc808844f38078a4e
iPwnds <florian.braun.2005@gmail.com>
2023-01-13 00:11:21 +0100

    new header (experimental)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,59 +1,51 @@
 <template>
 <div id="progressbar"></div>
-  <header>
+<header>
+  <nav>
     <div class="topnav" data-aos="fade-in">
-      <p class="quote rainbow" data-aos="fade-in">Never Give Up</p>
       <a href="index.html" data-aos="fade-in"><b>// Florian's Portfolio</b></a>
       <a href="about.html" data-aos="fade-in">// About</a>
       <a href="projects.html" data-aos="fade-in">// Projects</a>
       <a href="contact.html" data-aos="fade-in">// Contact</a>
     </div>
-  </header>
+  </nav>
+    <a href="https://www.youtube.com/watch?v=43TmnIaL3n4" class="quote gradient" data-aos="fade-in">Never<br>Give<br>Up</a>
+</header>
 </template>
 
 <style scoped>
   header {
     overflow: hidden;
+    padding: 2vw 2vw 10vh 4vw;
+    max-width: 100%;
+    display: inline-flex;
+  }
+
+  nav {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    text-shadow: none;
-    padding: 2vw 2vw 10vh 2vw;
+    align-items: left;
+    justify-content: left;
   }
 
   .topnav {
     background-color: var(--midnight-blue-800);
     border-radius: 20px;
     line-height: 2.5;
-    text-shadow: none;
-    padding: 0.25vw 1vw 1vw 0.75vw;
+    padding: 1vw 1vw 1vw 0.75vw;
   }
 
-  header a {
+  .topnav a {
     float: left;
     color: var(--midnight-blue-400);
     padding: 0.75vw 3vw 0.75vw 3vw;
     line-height: 2.5;
-    text-decoration: none;
     font-size: 1.5vw;
   }
 
-  header a:hover {
+  .topnav a:hover {
     background-color: var(--midnight-blue-600);
     border-radius: 20px;
-    text-shadow: none;
     box-shadow: 3px 2px 2px var(--midnight-blue-800);
-  }
-
-  @keyframes animate {
-  	0%,100%
-  	{
-    filter: hue-rotate(0deg);
-  	}
-  	50%
-  	{
-    filter: hue-rotate(360deg);
-  	}
   }
 
   #progressbar {
@@ -76,17 +68,29 @@
   	filter: blur(10px);
   }
 
-  .quote {
-    text-align: left;
-    font-size: 0.5vw;
+  @keyframes animate {
+  	0%,100%
+  	{
+    filter: hue-rotate(0deg);
+  	}
+  	50%
+  	{
+    filter: hue-rotate(360deg);
+  	}
   }
 
-  .rainbow {
-    background: linear-gradient(to right, #62efab 5%, #F2EA7D 15%, #F2EA7D 25%, #FF8797 35%, #FF8797 45%, #e1a4f4 55%, #e1a4f4 65%, #82fff4 75%, #82fff4 85%, #62efab 95%);
-    background-size: 200%;
+  .quote {
+    font-size: 2vw;
+    display: flex;
+    margin-left: 25vw;
+    text-align: right;
+  }
+
+  .gradient {
+    background: linear-gradient(45deg, #CA4246 16.666%, #E16541 16.666%, #E16541 33.333%, #F18F43 33.333%, #F18F43 50%, #8B9862 50%, #8B9862 66.666%, #476098 66.666%, #476098 83.333%, #A7489B 83.333%);
+    background-size: 100%;
     -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    animation: bar 15s linear infinite;
+    -webkit-text-fill-color: transparent; 
   }
 </style>
 
