fa5194bc2000cb91fd3752379e0f559378c7a290
iPwnds <florian.braun.2005@gmail.com>
2023-04-15 16:35:02 +0200

    do i need this?

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: 'public',
   build: {
-    outDir: '../dist',
     rollupOptions: {
       input: {
         main: ('public/index.html'),
