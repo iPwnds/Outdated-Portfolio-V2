3227496e13489f8c565fa45f5e8c01343425eaef
iPwnds <florian.braun.2005@gmail.com>
2023-04-15 16:35:56 +0200

    Revert "do i need this?"
    This reverts commit fa5194bc2000cb91fd3752379e0f559378c7a290.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: 'public',
   build: {
+    outDir: '../dist',
     rollupOptions: {
       input: {
         main: ('public/index.html'),
