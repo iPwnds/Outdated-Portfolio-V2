d57bf41988c9c941974a3e638adac43374b38ccb
iPwnds <florian.braun.2005@gmail.com>
2023-05-06 11:25:54 +0200

    button changes and animations

--- public/src/components/Header.vue
+++ public/src/components/Header.vue
@@ -16,16 +16,6 @@
 </template>
 
 <style scoped>
-  @media screen and (max-width: 900px) {
-    .topnav a {
-      font-size: 3vw;
-    }
-
-    .quote {
-      font-size: 4vw;
-    }
-  }
-
   header {
     overflow: hidden;
     padding: 2vw 2vw 10vh 2vw;
@@ -40,7 +30,7 @@
   }
 
   .topnav {
-    background-color: var(--midnight-blue-800);
+    background-color: var(--midnight-blue-900);
     border-radius: 20px;
     line-height: 2.5;
     padding: 1vw 1vw 1vw 0.75vw;
@@ -55,40 +45,47 @@
   }
 
   .topnav a:hover {
-    background-color: var(--midnight-blue-600);
+    background-color: var(--midnight-blue-400);
     border-radius: 20px;
-    box-shadow: 3px 2px 2px var(--midnight-blue-800);
+    color: var(--midnight-blue-900);;
   }
 
   #progressbar {
-	  position: fixed;
-	  top: 0;
-	  left: 0;
-	  width: 5px;
-	  background: linear-gradient(to top, #62efab,#62efab);
-	  animation: animate 10s linear infinite;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 5px;
+    background: linear-gradient(to top, #62efab,#62efab);
+    animation: animate 10s linear infinite;
   }
 
   #progressbar :before {
-  	content: '';
-  	position: absolute;
-  	top: 0;
-  	left: 0;
-  	width: 100%;
-  	height: 100%;
-  	background: linear-gradient(to top, #62efab, #62efab);
-  	filter: blur(10px);
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to top, #62efab, #62efab);
+    filter: blur(10px);
   }
 
   @keyframes animate {
-  	0%,100%
-  	{
-    filter: hue-rotate(0deg);
-  	}
-  	50%
-  	{
-    filter: hue-rotate(360deg);
-  	}
+    0%,100% {
+      filter: hue-rotate(0deg);
+    }
+    50% {
+      filter: hue-rotate(360deg);
+    }
+  }
+
+  @keyframes fade {
+    0% {
+      background-size: 100%;
+    }
+    100% {
+      background-size: 200%;
+    }
   }
 
   .quote {
@@ -106,8 +103,22 @@
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent; 
   }
+
+  .gradient:hover {
+    background: linear-gradient(45deg, #CA4246 16.666%, #E16541 16.666%, #E16541 33.333%, #F18F43 33.333%, #F18F43 50%, #8B9862 50%, #8B9862 66.666%, #476098 66.666%, #476098 83.333%, #A7489B 83.333%);
+    background-size: 200%;
+    animation: fade ease 2.5s;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+  }
 </style>
 
 <script setup lang="ts">
+const progress = document.getElementById('progressbar')
+const totalHeight = document.body.scrollHeight - window.innerHeight
 
+window.onscroll = function () {
+  const progressHeight = (window.pageYOffset / totalHeight) * 100
+  progress.style.height = progressHeight + '%'
+}
 </script>
