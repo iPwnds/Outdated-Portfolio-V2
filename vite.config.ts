71033b251a42c80de21c5d3916050f6af39f5e0d
iPwnds <florian.braun.2005@gmail.com>
2023-05-18 18:50:16 +0200

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: {
-        main: ('public/index.html'),
-        contact: ('public/contact.html'),
-        404: ('public/404.html'),
-      },
-    },
-  },
 })
